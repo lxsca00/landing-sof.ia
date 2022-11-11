a21535f095a54c4ee131dcd54926bc0db6f310e8
lxsca00 <ljimena10@gmail.com>
2022-11-11 18:20:10 -0500

    add video

--- src/pages/FourthPage/index.js
+++ src/pages/FourthPage/index.js
@@ -1,6 +1,7 @@
 import { PageName } from "../../components/PageName";
 import doll from "../../assets/Muñequito.png";
 import gif from "../../assets/giphy.gif";
+import video from "../../assets/video.mp4"
 import style from "./FourthPage.module.css"
 
 const FourthPage = () => {
@@ -11,6 +12,7 @@ const FourthPage = () => {
         <img src={doll} alt="doll"></img>
         <img src={gif} alt="gif"></img>
       </div>
+      <video src={video}></video>
     </section>
   );
 };
